92ecdd1467755e1b23c4849e0e7a37c6dadd6562
tpolvinen <tatu.polvinen@gmail.com>
2020-09-29 21:42:14 +0300

    Updates to elpa/

---
+++ .emacs.d/elpa/ein-20200926.403/ein-pkg.el
@@ -0,0 +1,14 @@
+(define-package "ein" "20200926.403" "Emacs IPython Notebook"
+  '((emacs "25")
+    (websocket "20190620")
+    (anaphora "20180618")
+    (request "20200117")
+    (deferred "0.5")
+    (polymode "20190714")
+    (exec-path-from-shell "20191229")
+    (dash "2.13.0")
+    (with-editor "20200522"))
+  :commit "1bfb80c8bbe4ace10c4b430c493149efa4aabbbd")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
